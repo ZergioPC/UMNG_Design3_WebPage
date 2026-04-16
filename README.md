c342238254f338fcf1bcf46dbc842f6eb3165a84
Sergio Palacios <sergid26pal@gmail.com>
2026-04-15 22:47:48 -0500

    init commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Web para el proyecto de curso de Diseño 3
---
+++ astro.config.mjs
@@ -0,0 +1,18 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
+  },
+});
